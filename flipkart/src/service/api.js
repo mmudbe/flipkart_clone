160a78236cc099e0c7f4e729001e7ff0cb918c40
Mahadev Mudbe <mmudbecodes@gmail.com>
2023-05-04 18:58:30 +0530

    backend link added

--- flipkart/src/service/api.js
+++ flipkart/src/service/api.js
@@ -1,6 +1,6 @@
 // try fetch in javascript we are using axios
 import axios from 'axios';
-const URL = 'http://localhost:8000';
+const URL = 'https://flipkart-clone-api.vercel.app/';
 // asyncronous request
 export const authenticateSignup =async (data) => {
     try {
